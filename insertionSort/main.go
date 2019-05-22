2bf9d71dbd4428615836bfb4e60712ab7eebd256
zumi <aponmaru@gmail.com>
2019-05-22 20:24:50 +0900

    [add] insertionSort 追加
    昇順のみ

---
+++ insertionSort/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := [6]int{78, 23, 123, 55, 33, 99}
+	var i, c, temp int
+
+	// ソート前の配列表示
+	fmt.Println(arr)
+
+	// 昇順でソート
+	for i = 1; i < len(arr); i++ {
+		temp = arr[i]
+		for c = i - 1; c >= 0; c-- {
+			if arr[c] > temp {
+				arr[c + 1] = arr[c]
+			} else {
+				break
+			}
+		}
+		arr[c + 1] = temp
+	}
+
+	// ソート結果を表示
+	fmt.Println(arr)
+}
