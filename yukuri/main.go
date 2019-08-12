79d8f0a2236e0ed8686c8c26037f3afc94206cb0
zumi <aponmaru@gmail.com>
2019-08-12 19:17:17 +0900

    [fix] ユークリッドの互除法の README.md に使い方追加

--- yukuri/main.go
+++ yukuri/main.go
@@ -10,14 +10,14 @@ func main() {
 	// 引数がなければエラー
 	c := len(os.Args) - 1
 	if c < 2 {
-		fmt.Fprintln(os.Stderr, "err")
+		fmt.Fprintln(os.Stderr, "引数が整数で二つ以上必要です。example: ./yukuri foo bar")
 		os.Exit(1)
 	}
 
 	a, err := strconv.Atoi(os.Args[1])
 	b, err := strconv.Atoi(os.Args[2])
 	if err != nil {
-		fmt.Println(b)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
